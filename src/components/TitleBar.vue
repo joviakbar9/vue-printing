dcaa8a15a77f902d699679d9b9ca032518292abf
joviakbar9 <joviakbar9@gmail.com>
2021-11-30 20:43:43 +0700

    upload file

---
+++ src/components/TitleBar.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="p-6">
+    <level>
+      <ul>
+        <li
+          v-for="(title, index) in titleStack"
+          :key="index"
+          class="title-stack-item inline-block pr-3 text-2xl text-gray-500 dark:text-gray-400 last:pr-0 last:font-black last:text-black
+            dark:text-gray-100">
+          {{ title }}
+        </li>
+      </ul>
+      <jb-button
+        href=""
+        color="info"
+        label="Pemesanan"
+        target="_blank"
+      />
+    </level>
+  </section>
+</template>
+
+<script>
+import { mdiGithub } from '@mdi/js'
+import Level from '@/components/Level.vue'
+import JbButton from '@/components/JbButton.vue'
+
+export default {
+  name: 'TitleBar',
+  components: {
+    Level,
+    JbButton
+  },
+  props: {
+    titleStack: {
+      type: Array,
+      default: () => []
+    }
+  },
+  setup () {
+    return {
+      mdiGithub
+    }
+  }
+}
+</script>
+
+<style scoped>
+li.title-stack-item:not(:last-child):after {
+  content: '/';
+  @apply inline-block pl-3;
+}
+</style>
---
+++ src/views/Tables.vue
@@ -0,0 +1,67 @@
+<template>
+  <title-bar :title-stack="titleStack" />
+  <hero-bar>Transaksi</hero-bar>
+  <main-section>
+    <card-component class="mb-6" title="Clients" :icon="mdiAccountMultiple" has-table>
+      <clients-table checkable/>
+    </card-component>
+
+    <title-sub-bar :icon="mdiTableBorder" title="Wrapped variation"/>
+
+    <notification color="success" :icon="mdiTableBorder">
+      <b>Tightly wrapped</b> &mdash; table header becomes card header
+    </notification>
+
+    <card-component class="mb-6" has-table>
+      <clients-table checkable/>
+    </card-component>
+
+    <title-sub-bar :icon="mdiTableOff" title="Empty variation"/>
+
+    <notification color="danger" :icon="mdiTableOff">
+      <b>Empty table.</b> When there's nothing to show
+    </notification>
+
+    <card-component empty/>
+  </main-section>
+
+  <bottom-other-pages-section />
+</template>
+
+<script>
+import { ref } from 'vue'
+import { mdiMonitorCellphone, mdiAccountMultiple, mdiTableBorder, mdiTableOff } from '@mdi/js'
+import MainSection from '@/components/MainSection.vue'
+import Notification from '@/components/Notification.vue'
+import ClientsTable from '@/components/ClientsTable.vue'
+import CardComponent from '@/components/CardComponent.vue'
+import TitleBar from '@/components/TitleBar.vue'
+import HeroBar from '@/components/HeroBar.vue'
+import BottomOtherPagesSection from '@/components/BottomOtherPagesSection.vue'
+import TitleSubBar from '@/components/TitleSubBar.vue'
+
+export default {
+  name: 'Transaksi',
+  components: {
+    TitleSubBar,
+    MainSection,
+    HeroBar,
+    TitleBar,
+    CardComponent,
+    ClientsTable,
+    Notification,
+    BottomOtherPagesSection
+  },
+  setup () {
+    const titleStack = ref(['Admin', 'Transaksi'])
+
+    return {
+      titleStack,
+      mdiMonitorCellphone,
+      mdiAccountMultiple,
+      mdiTableBorder,
+      mdiTableOff
+    }
+  }
+}
+</script>
